74e9f8f30b5e54ebee42838d936155dd862a87e5
valflrt <valflrt@pm.me>
2024-08-18 10:35:19 +0200

    Add pattern centering and screenshot

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::env;
 
-const CHARS: [char; 8] = ['─', '│', '╭', '╮', '╯', '╰', '┼', ' '];
+use termsize::Size;
+
+const CHARS: [char; 16] = [
+    '─', '│', '╭', '╮', '╯', '╰', '┼', '╴', '╵', '╶', '╷', '├', '┤', '┬', '┴', ' ',
+];
 // const CHARS: [char; 8] = ['═', '│', '╞', '╡', '╤', '╧', '╪', ' '];
 // const CHARS: [char; 8] = ['─', '│', '├', '┤', '┬', '┴', '┼', ' '];
 // const CHARS: [char; 8] = ['═', '║', '╠', '╣', '╦', '╩', '╬', ' '];
@@ -14,16 +18,30 @@ fn main() {
         }
         2 => {
             let hash = sha256::digest(&args[1]);
-            for (i, mut byte) in hash.bytes().enumerate() {
-                for _ in 0..3 {
-                    print!("{}", CHARS[(byte % 8) as usize]);
-                    byte /= 8;
+            let bytes = hash.bytes().collect::<Vec<_>>();
+
+            let start_i = termsize::get()
+                .map(|size: Size| (size.cols - 12) / 2)
+                .unwrap_or(2);
+
+            println!("\n");
+            for (i, bytes) in bytes.chunks_exact(2).enumerate() {
+                if i % 4 == 0 {
+                    print!("{}", " ".repeat(start_i as usize));
                 }
-                if (i + 1) % 8 == 0 {
+                if let &[a, b] = bytes {
+                    let (mut a, mut b) = (a, b);
+                    for _ in 0..3 {
+                        print!("{}", CHARS[(a % 8 + b % 8) as usize]);
+                        a /= 8;
+                        b /= 8;
+                    }
+                };
+                if (i + 1) % 4 == 0 {
                     println!("");
                 }
             }
-            println!("")
+            println!("\n");
         }
         _ => {
             println!("Too many arguments.");
