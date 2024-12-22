896dc5f97d20e05737d1d05224c1e258e6760770
valflrt <valflrt@pm.me>
2024-12-22 19:19:50 +0100

    Allow piping to shosha256

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-A sha256 previewer I made just for fun...
+This is a sha256 previewer I made just for fun...
 
-![a screenshot of the program](./img/screenshot.png)
+You can preview the sha256 hash of text:
+
+![a sha256 preview of "helo !"](./img/text.png)
+
+As well as data piped to it:
+
+![a sha256 preview of the data of Cargo.toml](./img/piped_data.png)
+
+You can install it on your computer by downloading the project repository and using the following command in its root directory (assuming you have cargo installed):
+
+```
+cargo install --path .
+```
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use std::env;
+use std::{
+    env,
+    io::{self, Read},
+};
 
+use atty;
 use termsize::Size;
 
 const CHARS: [char; 16] = [
@@ -14,37 +18,48 @@ fn main() {
 
     match args.len() {
         1 => {
-            println!("This is a sha256 previewer.");
+            if atty::is(atty::Stream::Stdin) {
+                println!("This is a sha256 previewer.");
+            } else {
+                let mut buf = Vec::new();
+                io::stdin().lock().read_to_end(&mut buf).unwrap();
+
+                print_pattern(&buf);
+            }
         }
         2 => {
-            let hash = sha256::digest(&args[1]);
-            let bytes = hash.bytes().collect::<Vec<_>>();
-
-            let start_i = termsize::get()
-                .map(|size: Size| (size.cols - 12) / 2)
-                .unwrap_or(2);
-
-            println!("\n");
-            for (i, bytes) in bytes.chunks_exact(2).enumerate() {
-                if i % 4 == 0 {
-                    print!("{}", " ".repeat(start_i as usize));
-                }
-                if let &[a, b] = bytes {
-                    let (mut a, mut b) = (a, b);
-                    for _ in 0..3 {
-                        print!("{}", CHARS[(a % 8 + b % 8) as usize]);
-                        a /= 8;
-                        b /= 8;
-                    }
-                };
-                if (i + 1) % 4 == 0 {
-                    println!("");
-                }
-            }
-            println!("\n");
+            print_pattern(&args[1].bytes().collect::<Vec<_>>());
         }
         _ => {
             println!("Too many arguments.");
         }
     }
+}
+
+fn print_pattern(input: &[u8]) {
+    let hash = sha256::digest(input);
+    let bytes = hash.bytes().collect::<Vec<_>>();
+
+    let start_i = termsize::get()
+        .map(|size: Size| (size.cols - 12) / 2)
+        .unwrap_or(2);
+
+    println!("\n");
+    for (i, bytes) in bytes.chunks_exact(2).enumerate() {
+        if i % 4 == 0 {
+            print!("{}", " ".repeat(start_i as usize));
+        }
+        if let &[a, b] = bytes {
+            let (mut a, mut b) = (a, b);
+            for _ in 0..3 {
+                print!("{}", CHARS[(a % 8 + b % 8) as usize]);
+                a /= 8;
+                b /= 8;
+            }
+        };
+        if (i + 1) % 4 == 0 {
+            println!("");
+        }
+    }
+    println!("\n");
 }
